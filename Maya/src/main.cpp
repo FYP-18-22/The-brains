21d1bb5664fe39fff8d0f27196c7ac2dd9d9417a
Mike-Kimani <mikalkimmani@gmail.com>
2022-10-26 22:25:41 +0300

    Testing interrupts

--- Maya/src/main.cpp
+++ Maya/src/main.cpp
@@ -1,26 +1,43 @@
 #include <Arduino.h>
 #include <defs.h>
 
-void setup() 
+bool state = true;
+
+void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(zc_interrupt, INPUT);
+  pinMode(PB10, INPUT);
   pinMode(pump_control, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(zc_interrupt), change_flow_rate, CHANGE);
+  pinMode(LED_BUILTIN, OUTPUT);
+  attachInterrupt(PB10, change_flow_rate, FALLING);
 }
 
-void loop() 
+void loop()
 {
-  // put your main code here, to run repeatedly:
-  wait_time = 2;
+  //put your main code here, to run repeatedly:
+  // wait_time = 2;
+  Serial.println(state);
+  if(state)
+  {
+    digitalWrite(PC13, LOW);
+  }
+  else
+  {
+    digitalWrite(PC13, HIGH);
+  }
+  delay(100);
+  
+
 }
 
 void change_flow_rate()
 {
-  delay(wait_time);
-  digitalWrite(pump_control, HIGH);
-  delay(5);
-  digitalWrite(pump_control, LOW);
-  Serial.print(millis());
+  // delay(wait_time);
+  // digitalWrite(pump_control, HIGH);
+  // delay(5);
+  // digitalWrite(pump_control, LOW);
+  // Serial.print(millis());
+  state != state;
+
 }
