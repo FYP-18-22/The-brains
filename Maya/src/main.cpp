d253457496e7de81f5cd4a66698be92ef7c12729
1998-felix <felix.gateru@gmail.com>
2022-11-06 07:43:06 +0300

    Add temp data from thermocouple
    Signed-off-by: 1998-felix <felix.gateru@gmail.com>

--- Maya/src/main.cpp
+++ Maya/src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
+#include "max6675.h" 
+
+int soPin = PB9;// SO=Serial Out
+int csPin = PB6;// CS = chip select CS pin
+int sckPin = PB5;// SCK = Serial Clock pin
+
+
+MAX6675 Module(sckPin, csPin, soPin);// create instance object of MAX6675
 
 void setup() {
-  // put your setup code here, to run once:
+
+          
+  Serial.begin(9600);// initialize serial monitor with 9600 baud
+  Serial.println("MAX6675"); 
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // basic readout test, just print the current temp
+   Serial.print("C = "); 
+   Serial.println(Module.readCelsius());
+   delay(1000);
 }
