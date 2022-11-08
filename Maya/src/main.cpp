6ba6a7826ae2dc16d8f1dbc91423e077e2b660d6
1998-felix <felix.gateru@gmail.com>
2022-11-08 21:49:55 +0300

    changes

--- Maya/src/main.cpp
+++ Maya/src/main.cpp
@@ -6,23 +6,15 @@ LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars
 void setup()
 {
   lcd.init();                      // initialize the lcd 
-  lcd.init();
-  // Print a message to the LCD.
   lcd.backlight();
   lcd.setCursor(0,0);
   lcd.print("Hello, world!");
-  lcd.setCursor(0,1);
-  lcd.printstr("Ywrobot Arduino!");
-   lcd.setCursor(0,2);
-  lcd.printstr("Arduino LCM IIC 2004");
-   lcd.setCursor(0,3);
-  lcd.printstr("Power By Ec-yuan!");
 }
 
 void loop()
 { 
   lcd.setCursor(0,0);
-  lcd.printstr("konichiwaa");
+  lcd.print("konichiwaa");
   delay(1000);
   lcd.clear();
   delay(500);
