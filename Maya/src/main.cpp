e5eb8e4e5b217bfddfde8892e334a8077f6766a1
1998-felix <felix.gateru@gmail.com>
2022-11-08 21:57:04 +0300

    changes

--- Maya/src/main.cpp
+++ Maya/src/main.cpp
@@ -13,18 +13,14 @@ DallasTemperature sensors(&oneWire);
 
 void setup(void) 
 { 
- // start serial port 
+
 Serial.begin(115200); 
-// Serial.println("Dallas Temperature IC Control Library Demo"); 
- // Start up the library 
+ 
  sensors.begin(); 
 } 
 void loop(void) 
 { 
- // call sensors.requestTemperatures() to issue a global temperature 
- // request to all devices on the bus 
-/********************************************************************/
- //Serial.print(" Requesting temperatures..."); 
+
  sensors.requestTemperatures(); // Send the command to get temperature readings 
  //Serial.println("DONE"); 
 /********************************************************************/
@@ -32,6 +28,5 @@ void loop(void)
  Serial.println(sensors.getTempCByIndex(0)); 
  Serial.print("Temp 2 is: "); 
  Serial.println(sensors.getTempCByIndex(1));// Why "byIndex"?  
-   // You can have more than one DS18B20 on the same bus.  
-   // 0 refers to the first IC on 
+   
 } 
