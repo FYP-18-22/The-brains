0a689b8adc891062b48ad3cf7d98dd921e93ff59
1998-felix <felix.gateru@gmail.com>
2022-11-08 22:33:54 +0300

    changes

--- Maya/src/main.cpp
+++ Maya/src/main.cpp
@@ -1,35 +1,37 @@
 #include <Arduino.h>
 #include "OneWire.h"
 #include "DallasTemperature.h"
-#include "max6675.h" 
+#include "max6675.h"
 
-//Pin definitions
-#define  soPin  PB9// SO=Serial Out
-#define  csPin  PB6// CS = chip select CS pin
-#define sckPin  PB5// SCK = Serial Clock pi
+// Pin definitions
+#define soPin PB9  // SO=Serial Out
+#define csPin PB6  // CS = chip select CS pin
+#define sckPin PB5 // SCK = Serial Clock pi
 #define ONE_WIRE_BUS PB10
 
-
-OneWire oneWire(ONE_WIRE_BUS);  
+// Object creation
+MAX6675 thermocouple(sckPin, csPin, soPin); // create instance object of MAX6675
+OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 
-void setup(void) 
-{ 
+void setup(void)
+{
 
-Serial.begin(115200); 
- 
- sensors.begin(); 
-} 
-void loop(void) 
-{ 
+  Serial.begin(115200);
 
- sensors.requestTemperatures(); // Send the command to get temperature readings 
- //Serial.println("DONE"); 
-/********************************************************************/
-  Serial.print("Temp 1 is: "); 
- Serial.println(sensors.getTempCByIndex(0)); 
- Serial.print("Temp 2 is: "); 
- Serial.println(sensors.getTempCByIndex(1));// Why "byIndex"?  
-   
+  sensors.begin();
+}
+void loop(void)
+{
+
+  sensors.requestTemperatures(); // Send the command to get temperature readings
+                                 // Serial.println("DONE");
+  /********************************************************************/
+  Serial.print("C = ");
+  Serial.println(thermocouple.readCelsius());
+  Serial.print("Temp 1 is: ");
+  Serial.println(sensors.getTempCByIndex(0));
+  Serial.print("Temp 2 is: ");
+  Serial.println(sensors.getTempCByIndex(1)); // Why "byIndex"?
 }
