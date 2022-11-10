ac015442b0da53168a45d331a84922b6654d0309
1998-felix <felix.gateru@gmail.com>
2022-11-10 03:11:29 +0300

    Add in program tests

--- Maya/src/main.cpp
+++ Maya/src/main.cpp
@@ -1,12 +1,9 @@
 #include <Arduino.h>
-#include <Wire.h> 
+#include <Wire.h>
 #include "OneWire.h"
 #include "DallasTemperature.h"
 #include "max6675.h"
 #include "LiquidCrystal_I2C.h"
-
-
-
 
 // Pin definitions
 #define soPin PB9  // SO=Serial Out
@@ -18,17 +15,16 @@
 MAX6675 thermocouple(sckPin, csPin, soPin); // create instance object of MAX6675
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
-LiquidCrystal_I2C lcd(0x27,20,4); 
-
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 void setup(void)
 {
 
   Serial.begin(115200);
-  lcd.init(); 
-  lcd.backlight();
-  lcd.print("Hello, world!");
   sensors.begin();
+  Serial.println("Beginning tests");
+  tests();
+  Serial.println("Tests complete...........")
 }
 void loop(void)
 {
@@ -42,4 +38,71 @@ void loop(void)
   Serial.println(sensors.getTempCByIndex(0));
   Serial.print("Temp 2 is: ");
   Serial.println(sensors.getTempCByIndex(1)); // Why "byIndex"?
+}
+
+bool tests()
+{
+  while (!lcdTest())
+  {
+  }
+  while (!thermocoupleTest())
+  {
+  }
+  while (!DS18B20Test())
+}
+
+bool lcdTest()
+{
+  Serial.prinntln("Running LCD test!!!!!!!")
+      lcd.init();
+  lcd.backlight();
+  lcd.print("Hello, world!");
+  Serial.println("LCD printing......")
+      delay(1000);
+  return true
+}
+bool thermocoupleTest()
+{
+  Serial.println("Running thermocouple test!!!!");
+  int count = 0;
+  while (count < 10)
+  {
+    delay(100);
+    if (thermocouple.readCelsius() > 0 && thermocouple.readCelsius < 50)
+    {
+      count++;
+      Serial.println("The value of coount is: ", count);
+    }
+    else
+    {
+      continue;
+    }
+  }
+  Serial.println("Thermocouple test complete !!!!!!");
+  return true;
+}
+
+bool DS18B20Test()
+{
+  Serial.println("Running DS18B20 test !!!!!!!!!!");
+  int count == 0;
+  while (count < 10)
+  {
+    delay(100);
+    while (sensors.getDS18Count != 2)
+    {
+      Serial.println("The DS18B20 sensors are not appropriately connected!!!!!!")
+    }
+    Serial.println("There are two DS18B20 sensors connected.") if (sensors.getTempCByIndex(0) 2 > 0 && sensors.getTempCByIndex(0) < 50 && sensors.getTempCByIndex(1) > 0 && sensors.getTempCByIndex(1))
+    {
+      count++;
+      Serial.println("The value of count is: ", count);
+    }
+    else
+    {
+      continue;
+    }
+  }
+  Serial.println("Exiting DS18B20 test.....!!!!!");
+  return true;
 }
