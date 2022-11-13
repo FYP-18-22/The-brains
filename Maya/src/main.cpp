84772a4bfe76ce0b97f60998669591d7885ff774
1998-felix <felix.gateru@gmail.com>
2022-11-13 08:16:42 +0300

    Replace bus with single instance

--- Maya/src/main.cpp
+++ Maya/src/main.cpp
@@ -9,19 +9,23 @@
 #define soPin PB9  // SO=Serial Out
 #define csPin PB6  // CS = chip select CS pin
 #define sckPin PB5 // SCK = Serial Clock pi
-#define ONE_WIRE_BUS PB10
+#define ONE_WIRE_0 PB12
+#define ONE_WIRE_1 PB13
 
 // Object creation
 MAX6675 thermocouple(sckPin, csPin, soPin); // create instance object of MAX6675
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
+OneWire oneWire(ONE_WIRE_0);
+OneWire oneWire1(ONE_WIRE_1);
+DallasTemperature sensor0(&oneWire);
+DallasTemperature sensor1(&oneWire1);
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 void setup(void)
 {
 
   Serial.begin(115200);
-  sensors.begin();
+  sensor0.begin();
+  sensor1.begin();
   Serial.println("Beginning tests");
   tests();
   Serial.println("Tests complete...........")
@@ -29,15 +33,16 @@ void setup(void)
 void loop(void)
 {
 
-  sensors.requestTemperatures(); // Send the command to get temperature readings
+  sensor0.requestTemperatures();
+  sensor1.requestTemperatures(); // Send the command to get temperature readings
                                  // Serial.println("DONE");
   /********************************************************************/
   Serial.print("C = ");
   Serial.println(thermocouple.readCelsius());
   Serial.print("Temp 1 is: ");
-  Serial.println(sensors.getTempCByIndex(0));
+  Serial.println(sensor0.getTempCByIndex(0));
   Serial.print("Temp 2 is: ");
-  Serial.println(sensors.getTempCByIndex(1)); // Why "byIndex"?
+  Serial.println(sensor1.getTempCByIndex(0));
 }
 
 bool tests()
@@ -53,13 +58,14 @@ bool tests()
 
 bool lcdTest()
 {
-  Serial.prinntln("Running LCD test!!!!!!!")
-      lcd.init();
+  Serial.println("Running LCD test!!!!!!!");
+  lcd.init();
   lcd.backlight();
   lcd.print("Hello, world!");
-  Serial.println("LCD printing......")
-      delay(1000);
-  return true
+  Serial.println("LCD printing......");
+  delay(1000);
+  return true;
+
 }
 bool thermocoupleTest()
 {
@@ -68,10 +74,11 @@ bool thermocoupleTest()
   while (count < 10)
   {
     delay(100);
-    if (thermocouple.readCelsius() > 0 && thermocouple.readCelsius < 50)
+    if (thermocouple.readCelsius() > 0 && thermocouple.readCelsius() < 50)
     {
       count++;
-      Serial.println("The value of coount is: ", count);
+      Serial.println("The value of coount is: ");
+      Serial.print(count);
     }
     else
     {
@@ -85,18 +92,20 @@ bool thermocoupleTest()
 bool DS18B20Test()
 {
   Serial.println("Running DS18B20 test !!!!!!!!!!");
-  int count == 0;
+  int count = 0;
   while (count < 10)
   {
     delay(100);
-    while (sensors.getDS18Count != 2)
-    {
-      Serial.println("The DS18B20 sensors are not appropriately connected!!!!!!")
-    }
-    Serial.println("There are two DS18B20 sensors connected.") if (sensors.getTempCByIndex(0) 2 > 0 && sensors.getTempCByIndex(0) < 50 && sensors.getTempCByIndex(1) > 0 && sensors.getTempCByIndex(1))
+    // while (sensor0.isConnected(&oneWire))
+    // {
+    //   Serial.println("The DS18B20 sensors are not appropriately connected!!!!!!")
+    // }
+    Serial.println("There are two DS18B20 sensors connected."); 
+    if (sensor0.getTempCByIndex(0) > 0 && sensor0.getTempCByIndex(0) < 50 && sensor1.getTempCByIndex(0) > 0 && sensor1.getTempCByIndex(0))
     {
       count++;
-      Serial.println("The value of count is: ", count);
+      Serial.println("The value of count is: ");
+      Serial.print(count);
     }
     else
     {
