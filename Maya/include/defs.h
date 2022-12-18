6529f1e6a043399c19c22b9496c5f00528bbd8e7
Mike-Kimani <mikalkimmani@gmail.com>
2022-12-18 08:13:05 +0300

    Callable flow rate test

--- Maya/include/defs.h
+++ Maya/include/defs.h
@@ -7,12 +7,13 @@ float wall_temperature;
 float intial_coolant_temperature;
 float final_coolant_temperature;
 
-#ifdef ARDUINO_ARCH_AVR
-#define zc_interrupt 0
-#define pump_control 7 
-#else
+// #ifdef ARDUINO_ARCH_AVR
+// #define zc_interrupt 0
+// #define pump_control 7 
+//#else
 #define zc_interrupt PB8
 #define pump_control PB14
-#endif
+#define fan_switch PB4
+// #endif
 
 int wait_time;
--- Maya/src/main.cpp
+++ Maya/src/main.cpp
@@ -1,10 +1,14 @@
 #include <Arduino.h>
 #include <defs.h>
 
-int firing_time = 1;
-int prev_time;
-int current_time;
-int initial_time;
+short int firing_time = 0;
+short int prev_time;
+short int current_time;
+short int initial_time;
+short int pump_flag = 0;
+void flow_rate_test();
+
+
 
 void setup()
 {
@@ -26,29 +30,56 @@ void loop()
 {
   //put your main code here, to run repeatedly:
   current_time = millis();
-  if((current_time - prev_time >= 8000) && (firing_time <= 7))
+  if((current_time - initial_time >= 8000) && (current_time - prev_time >= 3000) && (firing_time <= 4))
   {
     firing_time += 1; 
     prev_time = current_time;
+    Serial.println(firing_time);
   }
   else
   {
     firing_time = firing_time;
   }
+  
 }
 
 void change_flow_rate()
 {
-  if (current_time - initial_time <= 6000)
+  if(pump_flag = 0)
   {
-    digitalWrite(pump_control, HIGH);
+    firing_time = firing_time;
   }
   else
   {
-    delayMicroseconds(5500);
+    delay(firing_time);
     digitalWrite(pump_control, HIGH);
     delayMicroseconds(10);
     digitalWrite(pump_control, LOW);
   }
   // Serial.println("Mike");
+}
+
+void flow_rate_test()
+{
+  digitalWrite(fan_switch, HIGH);
+  current_time = millis();
+  for(int speed_boost = 0; speed_boost <= 7; speed_boost++)
+  {
+    digitalWrite(pump_control, HIGH);
+    delay(1000);
+  }
+  pump_flag = 1;
+
+  if((current_time - prev_time >= 3000) && (firing_time <= 4))
+  {
+    firing_time += 1; 
+    prev_time = current_time;
+    Serial.println(firing_time);
+  }
+  else
+  {
+    firing_time = firing_time;
+  }
+
+
 }
